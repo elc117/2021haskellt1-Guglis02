b1131a6bceea2278fdd0a95c17af3065ecf9f5be
Guglis02 <gustavomf02@gmail.com>
2021-06-09 09:06:55 -0300

    embelezamento do codigo

--- Main.hs
+++ Main.hs
@@ -2,12 +2,18 @@ import Text.Printf
 
 type Point     = (Float,Float)
 type Rect      = (Point,Float,Float)
-type Circle    = (Point,Float)
-
 
 -------------------------------------------------------------------------------
 -- Paletas
 -------------------------------------------------------------------------------
+
+-- Função que escolhe as cores da imagem
+-- Caso o input do usuário seja um número aleatório, a opção selecionada será a última
+choosePalette:: Int -> Int -> [(Int,Int,Int)]
+choosePalette x n
+ | x == 0 = creeperPalette n
+ | x == 1 = zombiePalette n
+ | otherwise = skeletonPalette n
 
 -- Paleta para o Creeper
 creeperPalette :: Int -> [(Int,Int,Int)]
@@ -26,7 +32,7 @@ skeletonPalette n = take n $ cycle [(176, 173, 173),(189, 189, 189),(168, 168, 1
 -------------------------------------------------------------------------------
 
 genRectsInLine :: Int -> Float -> Float -> [Rect]
-genRectsInLine n x y = [((m*w, p), w, h) | m <- [0..fromIntegral (n-1)], p <- [0,y/8..y/8*7]]
+genRectsInLine n x y = [((m*w, p), w, h) | m <- [0..fromIntegral (n-1)], p <- [0,h..h*7]]
   where (w,h) = (x/8,y/8)
 
 -------------------------------------------------------------------------------
@@ -58,56 +64,71 @@ svgElements :: (a -> String -> String) -> [a] -> [String] -> String
 svgElements func elements styles = concat $ zipWith func elements styles
 
 -------------------------------------------------------------------------------
--- Funções para gerar rostos
+-- Funções para gerar os rostos
 -------------------------------------------------------------------------------
+
+-- Função que escolhe a figura que será desenhada
+-- Caso o input do usuário seja um número aleatório, a opção selecionada será a última
+chooseFace :: Int -> Float -> Float -> String
+chooseFace x w h
+ | x == 0 = svgCreeperFace w h
+ | x == 1 = svgZombieFace w h
+ | otherwise = svgSkeletonFace w h
 
 svgCreeperFace :: Float -> Float -> String
 svgCreeperFace w h = 
   -- Olhos
-  svgRect ((w/8,h/4),w/4,h/4) "fill:rgb(1,1,1)" ++
-  svgRect ((w/8*5,h/4),w/4,h/4) "fill:rgb(1,1,1)" ++
+  svgRect ((w/8,h/4),w/4,h/4) color ++
+  svgRect ((w/8*5,h/4),w/4,h/4) color ++
   -- Boca
-  svgRect ((w/4,h/8*5),w/2,h/4) "fill:rgb(1,1,1)" ++
-  svgRect ((w/8*3,h/2),w/4,h/4) "fill:rgb(1,1,1)" ++
-  svgRect ((w/4,h/8*7),w/8,h/8) "fill:rgb(1,1,1)" ++
-  svgRect ((w/8*5,h/8*7),w/8,h/8) "fill:rgb(1,1,1)"
+  svgRect ((w/4,h/8*5),w/2,h/4) color ++
+  svgRect ((w/8*3,h/2),w/4,h/4) color ++
+  svgRect ((w/4,h/8*7),w/8,h/8) color ++
+  svgRect ((w/8*5,h/8*7),w/8,h/8) color
+  where 
+    color = "fill:rgb(1,1,1);"
 
 svgZombieFace :: Float -> Float -> String
 svgZombieFace w h = 
   -- Olhos
-  svgRect ((w/8,h/2),w/4,h/8) "fill:rgb(1,1,1)" ++
-  svgRect ((w/8*5,h/2),w/4,h/8) "fill:rgb(1,1,1)" ++
+  svgRect ((w/8,h/2),w/4,h/8) color ++
+  svgRect ((w/8*5,h/2),w/4,h/8) color ++
   -- Nariz
-  svgRect ((w/8*3,h/8*5),w/4,h/8) "fill:rgb(60, 89, 31)" ++
+  svgRect ((w/8*3,h/8*5),w/4,h/8) color2 ++
   -- Boca
-  svgRect ((w/4,h/4*3),w/8,h/4) "fill:rgb(60, 89, 31)" ++
-  svgRect ((w/8*5,h/4*3),w/8,h/4) "fill:rgb(60, 89, 31)"
+  svgRect ((w/4,h/4*3),w/8,h/4) color2 ++
+  svgRect ((w/8*5,h/4*3),w/8,h/4) color2
+  where 
+    color = "fill:rgb(1,1,1);"
+    color2 = "fill:rgb(1,1,1,0.7);"
 
 svgSkeletonFace :: Float -> Float -> String
 svgSkeletonFace w h = 
   -- Olhos
-  svgRect ((w/8,h/2),w/4,h/8) "fill:rgb(66, 66, 66)" ++
-  svgRect ((w/8*5,h/2),w/4,h/8) "fill:rgb(66, 66, 66)" ++
+  svgRect ((w/8,h/2),w/4,h/8) color ++
+  svgRect ((w/8*5,h/2),w/4,h/8) color ++
   -- Nariz
-  svgRect ((w/8*3,h/8*5),w/4,h/8) "fill:rgb(117, 117, 117)" ++
+  svgRect ((w/8*3,h/8*5),w/4,h/8) color2 ++
   -- Boca
-  svgRect ((w/8,h/4*3),w/4*3,h/8) "fill:rgb(66, 66, 66)"
-  
+  svgRect ((w/8,h/4*3),w/4*3,h/8) color
+  where 
+    color = "fill:rgb(66, 66, 66);" 
+    color2 = "fill:rgb(117, 117, 117);"
+
 -------------------------------------------------------------------------------
 -- Função principal que gera arquivo com imagem SVG
 -------------------------------------------------------------------------------
 
-
-teste :: Float -> Float -> IO ()
-teste w h = do
-  writeFile "figs.svg" $ svgstrs
+gerar :: Float -> Float -> Int -> Int -> IO ()
+-- option1 decide a paleta de cores
+-- option2 decide a figura
+gerar w h option1 option2 = do
+  writeFile "mob.svg" $ svgstrs
   where svgstrs = svgBegin w h ++ svgfigs ++ face ++ svgEnd
         svgfigs = svgElements svgRect rects (map svgStyle palette)
         rects = genRectsInLine nrects w h
-        palette = skeletonPalette nrects
-        face = svgSkeletonFace w h
-        nrects = 64
-        --(w,h) = (800,800) -- width,height da imagem SVG
-
-
+        palette = choosePalette option1 nrects
+        face = chooseFace option2 w h
+        nrects = 64 
+        
 
